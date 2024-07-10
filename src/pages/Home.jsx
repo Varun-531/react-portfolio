2a563f708bd62aecfdb6f47e4a6755a7d2e79138
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-07-10 07:53:19 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { FaGithub } from "react-icons/fa";
 import { PiGraduationCap } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import WordFadeIn from "../components/magicui/word-fade-in";
+import Globe from "../components/magicui/globe";
 
 const Home = () => {
   const scrollToEducation = () => {
@@ -16,7 +17,7 @@ const Home = () => {
     <div>
       <section className="md:h-[45vw] h-[90vh] md:p-5 md:flex justify-between items-center md:mt-0 mt-20">
         <img
-          src="src/assets/learning-concept-illustration.png"
+          src="assets/learning-concept-illustration.png"
           className="md:hidden md:w-[40vw]"
         />
         <div className="flex flex-col items-center justify-center md:w-[50vw] gap-3">
@@ -40,7 +41,7 @@ const Home = () => {
           </div>
         </div>
         <img
-          src="src/assets/learning-concept-illustration.png"
+          src="assets/learning-concept-illustration.png"
           className="hidden md:block md:w-[40vw]"
         />
       </section>
@@ -48,13 +49,13 @@ const Home = () => {
         <WordFadeIn words="Education" />
         <div className="md:flex  justify-around">
           <img
-            src="src\assets\book-lover-concept-illustration.png"
+            src="assets\book-lover-concept-illustration.png"
             className="md:w-[40vw]"
             alt=""
           />
           <div className="flex flex-col md:gap-6 gap-2">
             <div>
-              <h1 className="md:font-bold md:text-3xl font-semibold text-xl">
+              <h1 className="md:font-bold md:text-2xl font-semibold text-xl">
                 Bachelor of Technology - CSE
               </h1>
               <h2 className="md:text-xl text-lg pl-5 font-semibold text-gray-400">
@@ -64,11 +65,11 @@ const Home = () => {
                 Bhimavaram, AP
               </h2>
               <h2 className="pl-5 font-semibold text-lg md:text-xl">
-                Current CGPA: 9.0
+                Current CGPA - 9.0
               </h2>
             </div>
             <div>
-              <h1 className="md:font-bold md:text-3xl font-semibold text-xl">
+              <h1 className="md:font-bold md:text-2xl font-semibold text-xl">
                 Higher Secondary - MPC
               </h1>
               <h2 className="md:text-xl text-lg pl-5 font-semibold text-gray-400">
@@ -82,7 +83,7 @@ const Home = () => {
               </h2>
             </div>
             <div>
-              <h1 className="md:font-bold md:text-3xl font-semibold text-xl">
+              <h1 className="md:font-bold md:text-2xl font-semibold text-xl">
                 Secondary
               </h1>
               <h2 className="md:text-xl text-lg pl-5 font-semibold text-gray-400">
@@ -98,6 +99,15 @@ const Home = () => {
           </div>
         </div>
       </section>
+      {/* <section>
+        <div className="relative flex h-full w-full  items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl">
+          <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+            Varun
+          </span>
+          <Globe className="top-28" />
+          <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+        </div>
+      </section> */}
     </div>
   );
 };
